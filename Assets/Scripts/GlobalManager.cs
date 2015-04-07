aff98c10bf385742772013409eb2c09a12193dab
senart <gavril.tonev@gmail.com>
2015-04-07 13:31:09 +0300

    Removed some prefabs and added the previous NGUI sprites

---
+++ Assets/Scripts/GlobalManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalManager : MonoBehaviour {
+
+	public struct PlayerData{
+		int speed;
+		int power;
+		int protection;
+		string name;  //Sweet idea for users to give contraptions an in-game name
+	}
+
+	GameObject DATASTRUCTURE;  //Something...
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	public void UpdateStats()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

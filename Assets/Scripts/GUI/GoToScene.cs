deb2d02b7d0228086d77a765c1903ec2fedc3d4c
Oleh Stolyar <stolyaroleh@users.noreply.github.com>
2015-04-23 04:25:13 +0300

    Scene changes
    I am so going to hibernate soon

---
+++ Assets/Scripts/GUI/GoToScene.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoToScene : MonoBehaviour {
+
+	public string nameOfTheScene;
+	public SceneFadeInOut fader;
+	
+	public void OnClick() {
+		if (fader) {
+			fader.nextSceneName = nameOfTheScene;
+			fader.EndScene();
+		} else {
+			Application.LoadLevel(nameOfTheScene);
+		}
+	}
+}
---
+++ Assets/Scripts/GUI/ShowMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowMenu : MonoBehaviour {
+
+	public GameObject menuButtons;
+
+	void OnClick () {
+		menuButtons.SetActive(true);
+		Destroy(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
